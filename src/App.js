a4ff4236ee594dfbaa4022967ad2aced59834378
ahmet <ahmet55uluisik05@gmail.com>
2023-05-02 18:46:01 +0300

    improved App and styles

--- src/App.js
+++ src/App.js
@@ -3,15 +3,29 @@ import resim2 from "./assets/IMG-20200522-WA0039.jpg";
 import resim3 from "./assets/IMG-20200518-WA0017.jpg";
 import resim4 from "./assets/034.jpg";
 import resim5 from "./assets/035.jpg";
+import "./styles.css";
+import { useState } from "react";
+
+const Images = [resim1, resim2, resim3, resim4, resim5];
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(Images[0]);
+
   return (
     <div className="App">
       <div className="container">
-        <img src={resim1} alt="selected" className="selected" />
+        <h1>Handsome Boys</h1>
+        <img src={selectedImg} alt="selected" className="selected" />
       </div>
       <div className="imgContainer">
-        <img src="" alt="tantuni" className="" />
+        {Images.map((img, index) => (
+          <img
+            key={index}
+            src={img}
+            alt="ss"
+            onClick={() => setSelectedImg(img)}
+          />
+        ))}
       </div>
     </div>
   );
