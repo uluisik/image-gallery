29768268d1dea68e005d4540ac271bb28cf044e8
ahmet <ahmet55uluisik05@gmail.com>
2023-05-02 17:02:05 +0300

    the app component has been created and the images have been uploaded

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import resim1 from "./assets/IMG-20200522-WA0025.jpg";
+import resim2 from "./assets/IMG-20200522-WA0039.jpg";
+import resim3 from "./assets/IMG-20200518-WA0017.jpg";
+import resim4 from "./assets/034.jpg";
+import resim5 from "./assets/035.jpg";
+
 function App() {
-  return <div className="App">aaaa</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <img src={resim1} alt="selected" className="selected" />
+      </div>
+      <div className="imgContainer">
+        <img src="" alt="tantuni" className="" />
+      </div>
+    </div>
+  );
 }
 
 export default App;
